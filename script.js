939be9cda6e2740caa863d42630a83a6fafa0834
TerrierT <94978153+TerrierT@users.noreply.github.com>
2021-12-25 11:12:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,9 +59,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
+     byeSpeaker.speak(names[i]);
   } else {
-     window.helloSpeaker.speak(names[i]);
+     helloSpeaker.speak(names[i]);
   }
 }
 })();
